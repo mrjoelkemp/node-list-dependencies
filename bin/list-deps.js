90f4a004ea153b6e2ae061430cb364fbac85b658
Joel Kemp <jokemp@adobe.com>
2014-06-06 15:45:31 -0400

    Initial

---
+++ bin/list-deps.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var detective = require('node-detective-amd'),
+    filename = process.argv[2],
+    dir = require('node-dir'),
+    fs = require('fs');
+
+if (! filename) {
+  throw new Error('please supply a filename or directory');
+}
+
+function isDirectory(filename) {
+  return fs.lstatSync(filename).isDirectory();
+}
+
+function forAllJSFiles(directory, cb) {
+  dir.readFiles(directory,
+  {
+    match: /.js$/,
+    exclude: /^\./,
+  },
+  function (err, content, filename, next) {
+    if (err) throw err;
+
+    console.log('\n---\n', filename, '\n---');
+
+    cb(content);
+    next();
+  });
+}
+
+function printDependencies(fileSrc) {
+  var dependencies = detective(fileSrc);
+
+  dependencies.forEach(function(dependency) {
+    console.log(dependency);
+  });
+}
+
+if (isDirectory(filename)) {
+  forAllJSFiles(filename, printDependencies);
+
+} else {
+  // Print the file and its dependency list
+  var fileContents = fs.readFileSync(filename).toString();
+
+  printDependencies(fileContents);
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+Lists all module dependencies for a JS file or JS files within a directory
+
+### Usage
+
+`list-deps filename` or `list-deps directory`
+
+Not meant to be required. Really just a shell wrapper around [node-detective-amd](https://github.com/mrjoelkemp/node-detective-amd).
+
+CommonJS support soon.
